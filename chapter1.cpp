0e6aefd680123d36592f5d59d0822cf46490d53a
basketballCzm <1850226700@qq.com>
2019-06-17 20:47:29 +0800

    chapter3

--- chapter1.cpp
+++ chapter1.cpp
@@ -98,6 +98,7 @@ static int gettok() {
 class ExprAST {
    public:
     virtual ~ExprAST() {}
+    virtual Value *codegen() = 0;
 };
 
 //NumberExprAST - Expression class for numeric literals like "1.0"
@@ -106,6 +107,7 @@ class NumberExprAST : public ExprAST {
 
    public:
     NumberExprAST(double Val) : Val(Val) {}
+    virtual Value *codegen();
 };
 
 //VariableExprAST - Expression class for referencing a variable , like "a"
@@ -114,6 +116,7 @@ class VariableExprAST : public ExprAST {
 
    public:
     VariableExprAST(const std::string &Name) : Name(Name) {}
+    virtual Value *codegen();
 };
 
 //BinaryExprAST - Expression class for a binary operator
@@ -387,11 +390,60 @@ static std::unique_ptr<FunctionAST> ParseTopLevelExpr() {
     return nullptr;
 }
 
+static LLVMContext TheContext;
+static IRBuilder<> Builder(TheContext);
+static std::unique_ptr<Module> TheModule;
+static std::map<std::string, Value *> NameValues;
+
+Value *LogErrorV(const char *Str) {
+    LogError(Str);
+    return nullptr;
+}
+
+/// In the LLVM IR, numeric constants are represented with the ConstantFP class, Note that in the LLVM IR that constants are all uniqued together and shared. For this reason, the API uses the “foo::get(…)” idiom instead of “new foo(..)” or “foo::Create(..)”.
+Value *NumberExprAST::codegen() {
+    return ConstantFP::get(TheContext, APFloat(val));
+}
+
+Value *VariableExprAST::codegen() {
+    // look this variable up in the function
+    Value *V = NameValues[Name];
+    if (!V) {
+        LogErrorV("Unkonw variable name");
+    }
+
+    return V;
+}
+
+Value *BinaryExprAST::codegen() {
+    Value *L = LHS->codegen();
+    Value *R = RHS->codegen();
+
+    if (!L || !R) {
+        return nullptr;
+    }
+
+    switch (Op) {
+        case '+':
+            return Builder.CreateFAdd(L, R, "addtmp");
+        case '-':
+            return Builder.CreateFSub(L, R, "subtmp");
+        case '*':
+            return Builder.CreateFMul(L, R, "multmp");
+        case '< ':
+            L = Builder.CreateFCmpULT(L, R, "cmptmp");
+            return Builder.CreateUIToFP(L, Type::getDoubleTy(TheContext), "booltmp");
+        default:
+            return LogErrorV("invalid binary operator");
+    }
+}
+
 //===----------------------------------------------------------------------===//
 // Top-Level parsing
 //===----------------------------------------------------------------------===//
 
-static void HandleDefinition() {
+static void
+HandleDefinition() {
     if (ParseDefinition()) {
         fprintf(stderr, "Parsed a function definition. \n");
     } else {
