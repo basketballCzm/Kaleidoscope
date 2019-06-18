9a35c70b53e72be47ca1832b5c71007f130c78a9
basketballCzm <1850226700@qq.com>
2019-06-18 19:36:13 +0800

    chapter3

--- chapter1.cpp
+++ chapter1.cpp
@@ -128,6 +128,7 @@ class BinaryExprAST : public ExprAST {
    public:
     BinaryExprAST(char op, std::unique_ptr<ExprAST> LHS, std::unique_ptr<ExprAST> RHS)
         : Op(op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+    virtual Value *codegen();
 };
 
 //callExprAST - Expression class for function calls
@@ -138,6 +139,7 @@ class CallExprAST : public ExprAST {
    public:
     CallExprAST(const std::string &Callee, std::vector<std::unique_ptr<ExprAST>> Args)
         : Callee(Callee), Args(std::move(Args)) {}
+    virtual Value *codegen();
 };
 
 /// PrototypeAST - This class represents the "prototype" for a function,
@@ -152,6 +154,7 @@ class PrototypeAST {
         : Name(name), Args(std::move(Args)) {}
 
     const std::string &getName() const { return Name; }
+    virtual Value *codegen();
 };
 
 //FunctionAST - this class represents a function definition itself
@@ -162,6 +165,7 @@ class FunctionAST {
    public:
     FunctionAST(std::unique_ptr<PrototypeAST> Proto, std::unique_ptr<ExprAST> Body)
         : Proto(std::move(Proto)), Body(std::move(Body)) {}
+    virtual Value *codegen();
 };
 
 //===----------------------------------------------------------------------===//
@@ -432,10 +436,88 @@ Value *BinaryExprAST::codegen() {
             return Builder.CreateFMul(L, R, "multmp");
         case '< ':
             L = Builder.CreateFCmpULT(L, R, "cmptmp");
+            // Covert bool 0/1 to dlouble 0.0 or 1.0
             return Builder.CreateUIToFP(L, Type::getDoubleTy(TheContext), "booltmp");
         default:
             return LogErrorV("invalid binary operator");
     }
+}
+
+Value *CallExprAST::codegen() {
+    //look up the name in the global module table
+    Function *CalleeF = TheModule->getFunction(Callee);
+    if (!CalleeF) {
+        return LogErrorV("UnKonwn function referenced");
+    }
+
+    // If arguement mismatch error
+    if (CalleeF->arg_size() != Args.size()) {
+        return LogErrorV("Incorrect # arguments passed");
+    }
+
+    std::vector<Value *> ArgsV;
+    for (unsigned i = 0, e = Args.size(); i != e; i++) {
+        ArgsV.push_back(Args[i]->codegen());
+        if (!ArgsV.back()) {
+            return nullptr;
+        }
+    }
+
+    return Builder.CreateCall(CalleeF, ArgsV, "calltmp");
+}
+
+Function *PrototypeAST::codegen() {
+    //Make the function type double (double, double) etc
+    std::vector<Type *> Doubles(Args.size(), Type::getDoubleTy(TheContext));
+    FunctionType *FT = FunctionType::get(Type::getDoubleTy(TheContext), Doubles, false);
+    FunctionType *F  = Function::Create(FT, Function::ExternalLinkage, Name, TheModule.get());
+    // set names for all arguments
+    unsigned Idx = 0;
+    for (auto &Arg : F->args()) {
+        Arg.setName(Args[Idx++]);
+    }
+
+    return F;
+}
+
+Function *FunctionAST::codegen() {
+    //First, check for an existing function from a previous 'extern' declaration
+    Function *TheFunction = TheModule->getFunction(Proto->getName);
+    if (!TheFunction) {
+        TheFunction = Proto->codegen();
+    }
+
+    if (!TheFunction) {
+        return nullptr;
+    }
+
+    if (!TheFunction->empty()) {
+        return (Function *)LogErrorV("Function cannot be redefined.");
+    }
+
+    //Create a new basic block to start insertion into
+    BasicBlock *BB = BasicBlock::Create(TheContext, "entry", TheFunction);
+    //The second line then tells the builder that new instructions should be inserted into the end of the new basic block.
+    Builder.SetInsertPoint(BB);
+
+    //Record the funnction arguments in the NameValues map
+    NamedValues.clear();
+    //we add the function arguments to the NamedValues map (after first clearing it out) so that they’re accessible to VariableExprAST nodes.
+    for (auto &Arg : TheFunction->args()) {
+        NamedValues[Arg.getName()] = &Arg;
+    }
+
+    if (Value *RetVal = Body->codegen()) {
+        //Finish off the function
+        Builder.CreateRet(RetVal);
+
+        //Validate the generated code, checking for consistency
+        verifyFunction(*TheFunction);
+        return TheFunction;
+    }
+    //error reading body, remove function
+    TheFunction->eraseFromParent();
+    return nullptr;
 }
 
 //===----------------------------------------------------------------------===//
@@ -444,11 +526,14 @@ Value *BinaryExprAST::codegen() {
 
 static void
 HandleDefinition() {
-    if (ParseDefinition()) {
-        fprintf(stderr, "Parsed a function definition. \n");
+    if (auto FnAST = ParseDefinition()) {
+        if (auto *FnIR = FnAST->codegen()) {
+            fprintf(stderr, "Read function definition: ");
+            FnIR->print(errs());
+            fprintf(stderr, "\n");
+        }
     } else {
         // skip token for error recovery
-        getNextToken();
     }
 }
 
